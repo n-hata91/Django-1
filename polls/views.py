be4630fc9c4d61f396b7afef0ee699b124e25391
n-hata91 <htnoms5a@gmail.com>
2020-10-26 19:52:52 +0900

    chapter4-2

--- polls/views.py
+++ polls/views.py
@@ -2,33 +2,24 @@ from django.http import HttpResponseRedirect # , HttpResponse, Http404
 # from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    # ショートカット関数により省略
-    # template = loader.get_template('polls/index.html')
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    # context_object_name = '任意の名前'
+    template_name = 'polls/detail.html'
 
-def detail(request, question_id):
-    # 省略
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question': question})
-    # context = {'question': question}
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 # def vote(request, question_id):
 #     return HttpResponse("You're voting on question %s." % question_id)
