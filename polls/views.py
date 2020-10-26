b31b73517bb8b49cb5de4d3d9eff93f0bf32b7c2
n-hata91 <htnoms5a@gmail.com>
2020-10-26 09:48:59 +0900

    chapter3-3

--- polls/views.py
+++ polls/views.py
@@ -21,6 +21,7 @@ def detail(request, question_id):
     # except Question.DoesNotExist:
     #     raise Http404("Question does not exist")
     # return render(request, 'polls/detail.html', {'question': question})
+    # context = {'question': question}
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
