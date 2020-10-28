b16c7c7b885d0fb00ed2e0de6cd781453d19eec4
n-hata91 <htnoms5a@gmail.com>
2020-10-28 17:32:59 +0900

    make fixture dir

--- polls/models.py
+++ polls/models.py
@@ -17,13 +17,13 @@ class Question(models.Model):
     was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
-  Question = models.ForeignKey(Question, on_delete=models.CASCADE)
-  choice_text = models.CharField(max_length=200)
-  votes = models.IntegerField(default=0)
+    Question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
-  def __str__(self):
-      return self.choice_text
+    def __str__(self):
+        return self.choice_text
 
-  def was_published_recently(self):
-      now = timezone.now()
-      return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
